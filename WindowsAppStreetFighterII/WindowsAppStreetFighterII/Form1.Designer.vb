02e052c656fab9d126e34fcbce80e343952668e3
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2020-06-16 13:25:25 +0900

    LifeGauge Time PNG Shrink

--- WindowsAppStreetFighterII/WindowsAppStreetFighterII/Form1.Designer.vb
+++ WindowsAppStreetFighterII/WindowsAppStreetFighterII/Form1.Designer.vb
@@ -40,112 +40,114 @@ Partial Class Form1
         Me.Button13 = New System.Windows.Forms.Button()
         Me.Button14 = New System.Windows.Forms.Button()
         Me.Button15 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(143, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(87, 12)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(474, 426)
+        Me.PictureBox1.Size = New System.Drawing.Size(615, 426)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 226)
+        Me.Button1.Location = New System.Drawing.Point(2, 187)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(33, 23)
+        Me.Button1.Size = New System.Drawing.Size(24, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(51, 226)
+        Me.Button2.Location = New System.Drawing.Point(32, 187)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(33, 23)
+        Me.Button2.Size = New System.Drawing.Size(21, 23)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(90, 226)
+        Me.Button3.Location = New System.Drawing.Point(59, 187)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(33, 23)
+        Me.Button3.Size = New System.Drawing.Size(22, 23)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "Button3"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(12, 187)
+        Me.Button4.Location = New System.Drawing.Point(2, 161)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(33, 23)
+        Me.Button4.Size = New System.Drawing.Size(24, 23)
         Me.Button4.TabIndex = 4
         Me.Button4.Text = "Button4"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(51, 187)
+        Me.Button5.Location = New System.Drawing.Point(32, 161)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(33, 23)
+        Me.Button5.Size = New System.Drawing.Size(21, 23)
         Me.Button5.TabIndex = 5
         Me.Button5.Text = "Button5"
         Me.Button5.UseVisualStyleBackColor = True
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(90, 187)
+        Me.Button6.Location = New System.Drawing.Point(59, 161)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(33, 23)
+        Me.Button6.Size = New System.Drawing.Size(22, 23)
         Me.Button6.TabIndex = 6
         Me.Button6.Text = "Button6"
         Me.Button6.UseVisualStyleBackColor = True
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(12, 142)
+        Me.Button7.Location = New System.Drawing.Point(2, 132)
         Me.Button7.Name = "Button7"
-        Me.Button7.Size = New System.Drawing.Size(33, 23)
+        Me.Button7.Size = New System.Drawing.Size(24, 23)
         Me.Button7.TabIndex = 7
         Me.Button7.Text = "Button7"
         Me.Button7.UseVisualStyleBackColor = True
         '
         'Button8
         '
-        Me.Button8.Location = New System.Drawing.Point(51, 142)
+        Me.Button8.Location = New System.Drawing.Point(32, 132)
         Me.Button8.Name = "Button8"
-        Me.Button8.Size = New System.Drawing.Size(33, 23)
+        Me.Button8.Size = New System.Drawing.Size(21, 23)
         Me.Button8.TabIndex = 8
         Me.Button8.Text = "Button8"
         Me.Button8.UseVisualStyleBackColor = True
         '
         'Button9
         '
-        Me.Button9.Location = New System.Drawing.Point(90, 142)
+        Me.Button9.Location = New System.Drawing.Point(59, 132)
         Me.Button9.Name = "Button9"
-        Me.Button9.Size = New System.Drawing.Size(33, 23)
+        Me.Button9.Size = New System.Drawing.Size(22, 23)
         Me.Button9.TabIndex = 9
         Me.Button9.Text = "Button9"
         Me.Button9.UseVisualStyleBackColor = True
         '
         'Button10
         '
-        Me.Button10.Location = New System.Drawing.Point(643, 174)
+        Me.Button10.Location = New System.Drawing.Point(708, 145)
         Me.Button10.Name = "Button10"
-        Me.Button10.Size = New System.Drawing.Size(33, 23)
+        Me.Button10.Size = New System.Drawing.Size(22, 23)
         Me.Button10.TabIndex = 10
         Me.Button10.Text = "Button10"
         Me.Button10.UseVisualStyleBackColor = True
         '
         'Button11
         '
-        Me.Button11.Location = New System.Drawing.Point(682, 174)
+        Me.Button11.Location = New System.Drawing.Point(736, 145)
         Me.Button11.Name = "Button11"
-        Me.Button11.Size = New System.Drawing.Size(33, 23)
+        Me.Button11.Size = New System.Drawing.Size(22, 23)
         Me.Button11.TabIndex = 11
         Me.Button11.Text = "Button11"
         Me.Button11.UseVisualStyleBackColor = True
@@ -157,45 +159,63 @@ Partial Class Form1
         '
         'Button12
         '
-        Me.Button12.Location = New System.Drawing.Point(721, 174)
+        Me.Button12.Location = New System.Drawing.Point(764, 145)
         Me.Button12.Name = "Button12"
-        Me.Button12.Size = New System.Drawing.Size(33, 23)
+        Me.Button12.Size = New System.Drawing.Size(24, 23)
         Me.Button12.TabIndex = 12
         Me.Button12.Text = "Button12"
         Me.Button12.UseVisualStyleBackColor = True
         '
         'Button13
         '
-        Me.Button13.Location = New System.Drawing.Point(643, 203)
+        Me.Button13.Location = New System.Drawing.Point(708, 174)
         Me.Button13.Name = "Button13"
-        Me.Button13.Size = New System.Drawing.Size(33, 23)
+        Me.Button13.Size = New System.Drawing.Size(22, 23)
         Me.Button13.TabIndex = 13
         Me.Button13.Text = "Button13"
         Me.Button13.UseVisualStyleBackColor = True
         '
         'Button14
         '
-        Me.Button14.Location = New System.Drawing.Point(682, 203)
+        Me.Button14.Location = New System.Drawing.Point(736, 174)
         Me.Button14.Name = "Button14"
-        Me.Button14.Size = New System.Drawing.Size(33, 23)
+        Me.Button14.Size = New System.Drawing.Size(22, 23)
         Me.Button14.TabIndex = 14
         Me.Button14.Text = "Button14"
         Me.Button14.UseVisualStyleBackColor = True
         '
         'Button15
         '
-        Me.Button15.Location = New System.Drawing.Point(721, 203)
+        Me.Button15.Location = New System.Drawing.Point(764, 174)
         Me.Button15.Name = "Button15"
-        Me.Button15.Size = New System.Drawing.Size(33, 23)
+        Me.Button15.Size = New System.Drawing.Size(24, 23)
         Me.Button15.TabIndex = 15
         Me.Button15.Text = "Button15"
         Me.Button15.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(54, 19)
+        Me.TextBox1.TabIndex = 16
+        Me.TextBox1.Text = "100"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 37)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(54, 19)
+        Me.TextBox2.TabIndex = 17
+        Me.TextBox2.Text = "100"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button15)
         Me.Controls.Add(Me.Button14)
         Me.Controls.Add(Me.Button13)
@@ -216,6 +236,7 @@ Partial Class Form1
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -236,4 +257,6 @@ Partial Class Form1
     Friend WithEvents Button13 As Button
     Friend WithEvents Button14 As Button
     Friend WithEvents Button15 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- WindowsAppStreetFighterII/WindowsAppStreetFighterII/Form1.vb
+++ WindowsAppStreetFighterII/WindowsAppStreetFighterII/Form1.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form1
+    Dim Time As Integer = 99
+    Dim Life As Integer = 100
+    Dim Life2 As Integer = 100
+    Dim frame As Integer = 0
     Dim cx As Integer = 0
     Dim cy As Integer = 0
     Dim jump As Integer = 0
@@ -19,6 +23,10 @@
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        frame = frame + 1
+        If frame Mod 100 = 0 Then
+            Time = Time - 1
+        End If
         PictureBox1.BackColor = Color.Black
         Dim canvas As New Bitmap(PictureBox1.Width, PictureBox1.Height)
         Dim g As Graphics = Graphics.FromImage(canvas)
@@ -45,9 +53,25 @@
                 cy = 0
             End If
         End If
-        g.DrawImage(img, 20 + cx, 220 - cy, 200, 100)
+        DrawTime(g)
+        g.DrawImage(img, 20 + cx, 220 - cy, 200, 200)
         g.Dispose()
         PictureBox1.Image = canvas
+    End Sub
+    Private Sub DrawTime(ByRef g As Graphics)
+        Dim f As Font = New Font("MSゴシック", 20)
+        Dim b As Brush = Brushes.White
+        Dim p As Point = New Point(PictureBox1.Width / 2 - 30, 10)
+        g.DrawString(Time.ToString, f, b, p)
+        Dim r1 As Rectangle = New Rectangle(10, 10, 200, 30)
+        g.FillRectangle(Brushes.Red, r1)
+        Dim r2 As Rectangle = New Rectangle(210 - Life * 2, 10, Life * 2, 30)
+        g.FillRectangle(Brushes.Yellow, r2)
+        Dim a As Integer = PictureBox1.Width
+        Dim r3 As Rectangle = New Rectangle(a - 210, 10, 200, 30)
+        g.FillRectangle(Brushes.Red, r3)
+        Dim r4 As Rectangle = New Rectangle(a - 210, 10, Life2 * 2, 30)
+        g.FillRectangle(Brushes.Yellow, r4)
     End Sub
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
         If jump = 0 Then
@@ -98,5 +122,35 @@
     End Sub
     Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
         state = 2
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim t As String = TextBox1.Text.ToString
+        If t = "" Then
+            Life = 0
+        Else
+            Life = t
+        End If
+        If Life < 0 Then
+            Life = 0
+        End If
+        If Life > 100 Then
+            Life = 100
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Dim t As String = TextBox2.Text.ToString
+        If t = "" Then
+            Life2 = 0
+        Else
+            Life2 = t
+        End If
+        If Life2 < 0 Then
+            Life2 = 0
+        End If
+        If Life2 > 100 Then
+            Life2 = 100
+        End If
+
     End Sub
 End Class
